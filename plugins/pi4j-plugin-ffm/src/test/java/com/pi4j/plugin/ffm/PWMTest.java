82bbc7148f1df3cd056164eb64fd79a86a81f8d2
digitalsmile <digitalsmile.dig@gmail.com>
2025-07-01 09:31:34 +0300

    #454 fix tests

--- plugins/pi4j-plugin-ffm/src/test/java/com/pi4j/plugin/ffm/PWMTest.java
+++ plugins/pi4j-plugin-ffm/src/test/java/com/pi4j/plugin/ffm/PWMTest.java
@@ -30,13 +30,13 @@ public class PWMTest {
 
     @Test
     public void testCreation() {
-        var pwmEnable = new FileDescriptorNativeMock.FileDescriptorTestData("/sys/class/pwm/pwmchip0/pwm1/enable", 1, "Test".getBytes(), (_) -> "1".getBytes());
-        var pwmDutyCycle = new FileDescriptorNativeMock.FileDescriptorTestData("/sys/class/pwm/pwmchip0/pwm1/duty_cycle", 2, "1".getBytes());
-        var pwmPolarity = new FileDescriptorNativeMock.FileDescriptorTestData("/sys/class/pwm/pwmchip0/pwm1/polarity", 3, "normal".getBytes());
-        var pwmPeriod = new FileDescriptorNativeMock.FileDescriptorTestData("/sys/class/pwm/pwmchip0/pwm1/period", 4, "1".getBytes());
+        var pwmEnable = new FileDescriptorNativeMock.FileDescriptorTestData("/sys/class/pwm/pwmchip0/pwm0/enable", 1, "Test".getBytes(), (_) -> "1".getBytes());
+        var pwmDutyCycle = new FileDescriptorNativeMock.FileDescriptorTestData("/sys/class/pwm/pwmchip0/pwm0/duty_cycle", 2, "1".getBytes());
+        var pwmPolarity = new FileDescriptorNativeMock.FileDescriptorTestData("/sys/class/pwm/pwmchip0/pwm0/polarity", 3, "normal".getBytes());
+        var pwmPeriod = new FileDescriptorNativeMock.FileDescriptorTestData("/sys/class/pwm/pwmchip0/pwm0/period", 4, "1".getBytes());
         try (var _ = FileDescriptorNativeMock.echo(pwmEnable, pwmDutyCycle, pwmPolarity, pwmPeriod)) {
             pi4j.pwm().create(PwmConfigBuilder.newInstance(pi4j)
-                .busNumber(1)
+                .busNumber(0)
                 .address(0)
                 .pwmType(PwmType.HARDWARE)
                 .build());
@@ -45,14 +45,14 @@ public class PWMTest {
 
     @Test
     public void testOnOff() {
-        var pwmEnable = new FileDescriptorNativeMock.FileDescriptorTestData("/sys/class/pwm/pwmchip0/pwm2/enable", 1, "Test".getBytes(), (_) -> "0".getBytes());
-        var pwmDutyCycle = new FileDescriptorNativeMock.FileDescriptorTestData("/sys/class/pwm/pwmchip0/pwm2/duty_cycle", 2, "1".getBytes());
-        var pwmPolarity = new FileDescriptorNativeMock.FileDescriptorTestData("/sys/class/pwm/pwmchip0/pwm2/polarity", 3, "normal".getBytes());
-        var pwmPeriod = new FileDescriptorNativeMock.FileDescriptorTestData("/sys/class/pwm/pwmchip0/pwm2/period", 4, "1".getBytes());
+        var pwmEnable = new FileDescriptorNativeMock.FileDescriptorTestData("/sys/class/pwm/pwmchip1/pwm0/enable", 1, "Test".getBytes(), (_) -> "0".getBytes());
+        var pwmDutyCycle = new FileDescriptorNativeMock.FileDescriptorTestData("/sys/class/pwm/pwmchip1/pwm0/duty_cycle", 2, "1".getBytes());
+        var pwmPolarity = new FileDescriptorNativeMock.FileDescriptorTestData("/sys/class/pwm/pwmchip1/pwm0/polarity", 3, "normal".getBytes());
+        var pwmPeriod = new FileDescriptorNativeMock.FileDescriptorTestData("/sys/class/pwm/pwmchip1/pwm0/period", 4, "1".getBytes());
         try (var _ = FileDescriptorNativeMock.echo(pwmEnable, pwmDutyCycle, pwmPolarity, pwmPeriod)) {
             var pwm = pi4j.pwm().create(PwmConfigBuilder.newInstance(pi4j)
-                .busNumber(2)
-                .address(0)
+                .busNumber(0)
+                .address(1)
                 .pwmType(PwmType.HARDWARE)
                 .build());
             pwm.on();
@@ -64,14 +64,14 @@ public class PWMTest {
 
     @Test
     public void testChangeFrequency() {
-        var pwmEnable = new FileDescriptorNativeMock.FileDescriptorTestData("/sys/class/pwm/pwmchip0/pwm3/enable", 1, "Test".getBytes(), (_) -> "0".getBytes());
-        var pwmDutyCycle = new FileDescriptorNativeMock.FileDescriptorTestData("/sys/class/pwm/pwmchip0/pwm3/duty_cycle", 2, "1".getBytes());
-        var pwmPolarity = new FileDescriptorNativeMock.FileDescriptorTestData("/sys/class/pwm/pwmchip0/pwm3/polarity", 3, "normal".getBytes());
-        var pwmPeriod = new FileDescriptorNativeMock.FileDescriptorTestData("/sys/class/pwm/pwmchip0/pwm3/period", 4, "1".getBytes());
+        var pwmEnable = new FileDescriptorNativeMock.FileDescriptorTestData("/sys/class/pwm/pwmchip2/pwm0/enable", 1, "Test".getBytes(), (_) -> "0".getBytes());
+        var pwmDutyCycle = new FileDescriptorNativeMock.FileDescriptorTestData("/sys/class/pwm/pwmchip2/pwm0/duty_cycle", 2, "1".getBytes());
+        var pwmPolarity = new FileDescriptorNativeMock.FileDescriptorTestData("/sys/class/pwm/pwmchip2/pwm0/polarity", 3, "normal".getBytes());
+        var pwmPeriod = new FileDescriptorNativeMock.FileDescriptorTestData("/sys/class/pwm/pwmchip2/pwm0/period", 4, "1".getBytes());
         try (var _ = FileDescriptorNativeMock.echo(pwmEnable, pwmDutyCycle, pwmPolarity, pwmPeriod)) {
             var pwm = pi4j.pwm().create(PwmConfigBuilder.newInstance(pi4j)
-                .busNumber(3)
-                .address(0)
+                .busNumber(0)
+                .address(2)
                 .pwmType(PwmType.HARDWARE)
                 .build());
             pwm.on();
