10599b60015dd91a1c2829e2e85722c0e2ca57d3
digitalsmile <digitalsmile.dig@gmail.com>
2026-06-11 15:01:36 +0300

    #643 JMH test extensions

--- plugins/pi4j-plugin-ffm/src/test/java/com/pi4j/plugin/jmh/I2CPerformanceTest.java
+++ plugins/pi4j-plugin-ffm/src/test/java/com/pi4j/plugin/jmh/I2CPerformanceTest.java
@@ -1,6 +1,8 @@
 package com.pi4j.plugin.jmh;
 
 import com.pi4j.Pi4J;
+import com.pi4j.io.i2c.I2CConfigBuilder;
+import com.pi4j.io.i2c.I2CImplementation;
 import com.pi4j.plugin.ffm.providers.i2c.FFMI2CProviderImpl;
 import org.openjdk.jmh.annotations.*;
 
@@ -18,39 +20,83 @@ public class I2CPerformanceTest {
 
     @Setup
     public void setup() throws InterruptedException, IOException {
-        var scriptPath = Paths.get("src/test/resources/i2c-setup.sh");
-        var setupScript = new ProcessBuilder("/bin/bash", "-c", "sudo " + scriptPath.toFile().getAbsolutePath()).start();
-        var result = setupScript.waitFor();
+        var scriptPath = Paths.get("src/test/resources/").toFile().getAbsoluteFile();
+        var setupScript = new ProcessBuilder("/bin/bash", "-c", "sudo " + scriptPath.getAbsolutePath() + "/i2c-setup.sh");
+        setupScript.directory(scriptPath);
+        var process = setupScript.start();
+        var result = process.waitFor();
         if (result != 0) {
             var username = System.getProperty("user.name");
-            var errorOutput = new String(setupScript.getErrorStream().readAllBytes());
+            var errorOutput = new String(process.getErrorStream().readAllBytes());
             fail("Failed to setup I2C Test: \n" + errorOutput + "\n" +
                 "Probably you need to add the I2C bash script to sudoers file " +
-                "with visudo: '" + username + " ALL=(ALL) NOPASSWD: " + scriptPath.toFile().getParentFile().getAbsolutePath() + "/'");
+                "with visudo: '" + username + " ALL=(ALL) NOPASSWD: " + scriptPath.getParentFile().getAbsolutePath() + "/'");
         }
     }
 
     @TearDown
     public void shutdown() throws InterruptedException, IOException {
-        var scriptPath = Paths.get("src/test/resources/i2c-clean.sh");
-        var setupScript = new ProcessBuilder("/bin/bash", "-c", "sudo " + scriptPath.toFile().getAbsolutePath()).start();
-        var result = setupScript.waitFor();
+        var scriptPath = Paths.get("src/test/resources/").toFile().getAbsoluteFile();
+        var setupScript = new ProcessBuilder("/bin/bash", "-c", "sudo " + scriptPath.getAbsolutePath() + "/i2c-clean.sh");
+        setupScript.directory(scriptPath);
+        var process = setupScript.start();
+        var result = process.waitFor();
         if (result != 0) {
             var username = System.getProperty("user.name");
-            var errorOutput = new String(setupScript.getErrorStream().readAllBytes());
+            var errorOutput = new String(process.getErrorStream().readAllBytes());
             fail("Failed to cleanup I2C Test: \n" + errorOutput + "\n" +
                 "Probably you need to add the I2C bash script to sudoers file " +
-                "with visudo: '" + username + " ALL=(ALL) NOPASSWD: " + scriptPath.toFile().getParentFile().getAbsolutePath() + "/'");
+                "with visudo: '" + username + " ALL=(ALL) NOPASSWD: " + scriptPath.getParentFile().getAbsolutePath() + "/'");
         }
     }
 
     @Benchmark
     @Warmup(iterations = 3)
-    public void testFFMSMBusRoundTrip() {
+    public void testSMBusRoundTrip() {
         var pi4j = Pi4J.newContextBuilder()
             .add(new FFMI2CProviderImpl())
             .build();
-        var i2c = pi4j.i2c().create(1, 0x1C);
+        var i2c = pi4j.i2c().create(I2CConfigBuilder
+            .newInstance(pi4j)
+            .bus(99)
+            .device(0x1C)
+            .i2cImplementation(I2CImplementation.SMBUS));
+        var writeBuffer = new byte[]{0x01, 0x02, 0x03};
+        i2c.writeRegister(0xFF, writeBuffer);
+        var readBuffer = new byte[3];
+        i2c.readRegister(0xFF, readBuffer);
+        pi4j.shutdown();
+    }
+
+    @Benchmark
+    @Warmup(iterations = 3)
+    public void testI2CDirectRoundTrip() {
+        var pi4j = Pi4J.newContextBuilder()
+            .add(new FFMI2CProviderImpl())
+            .build();
+        var i2c = pi4j.i2c().create(I2CConfigBuilder
+            .newInstance(pi4j)
+            .bus(99)
+            .device(0x1C)
+            .i2cImplementation(I2CImplementation.DIRECT));
+        var writeBuffer = new byte[]{0x01, 0x02, 0x03};
+        i2c.writeRegister(0xFF, writeBuffer);
+        var readBuffer = new byte[3];
+        i2c.readRegister(0xFF, readBuffer);
+        pi4j.shutdown();
+    }
+
+    @Benchmark
+    @Warmup(iterations = 3)
+    public void testI2CFileRoundTrip() {
+        var pi4j = Pi4J.newContextBuilder()
+            .add(new FFMI2CProviderImpl())
+            .build();
+        var i2c = pi4j.i2c().create(I2CConfigBuilder
+            .newInstance(pi4j)
+            .bus(99)
+            .device(0x1C)
+            .i2cImplementation(I2CImplementation.FILE));
         var writeBuffer = new byte[]{0x01, 0x02, 0x03};
         i2c.writeRegister(0xFF, writeBuffer);
         var readBuffer = new byte[3];
--- plugins/pi4j-plugin-ffm/src/test/java/com/pi4j/plugin/jmh/SPIPerformanceTest.java
+++ plugins/pi4j-plugin-ffm/src/test/java/com/pi4j/plugin/jmh/SPIPerformanceTest.java
@@ -20,32 +20,33 @@ public class SPIPerformanceTest {
 
     @Setup
     public static void setup() throws InterruptedException, IOException {
-        var scriptPath = Paths.get("src/test/resources/spi-setup.sh");
-        var setupScript = new ProcessBuilder("/bin/bash", "-c", "sudo " + scriptPath.toFile().getAbsolutePath()).start();
-        var result = setupScript.waitFor();
+        var scriptPath = Paths.get("src/test/resources").toFile().getAbsoluteFile();
+        var setupScript = new ProcessBuilder("/bin/bash", "-c", "sudo " + scriptPath + "/spi-setup.sh");
+        setupScript.directory(scriptPath);
+        var process = setupScript.start();
+        var result = process.waitFor();
         if (result != 0) {
             var username = System.getProperty("user.name");
-            var errorOutput = new String(setupScript.getErrorStream().readAllBytes());
-            var o = new String(setupScript.getInputStream().readAllBytes());
-            fail(o + "\n" + "Failed to setup SPI Test: \n" + errorOutput + "\n" +
+            var errorOutput = new String(process.getErrorStream().readAllBytes());
+            fail("Failed to setup SPI Test: \n" + errorOutput + "\n" +
                 "Probably you need to add the SPI bash script to sudoers file " +
-                "with visudo: '" + username + " ALL=(ALL) NOPASSWD: " + scriptPath.toFile().getParentFile().getAbsolutePath() + "/'");
+                "with visudo: '" + username + " ALL=(ALL) NOPASSWD: " + scriptPath.getParentFile().getAbsolutePath() + "/'");
         }
-
-
     }
 
     @TearDown
     public static void shutdown() throws InterruptedException, IOException {
-        var scriptPath = Paths.get("src/test/resources/spi-clean.sh");
-        var setupScript = new ProcessBuilder("/bin/bash", "-c", "sudo " + scriptPath.toFile().getAbsolutePath()).start();
-        var result = setupScript.waitFor();
+        var scriptPath = Paths.get("src/test/resources").toFile().getAbsoluteFile();
+        var setupScript = new ProcessBuilder("/bin/bash", "-c", "sudo " + scriptPath + "/spi-clean.sh");
+        setupScript.directory(scriptPath);
+        var process = setupScript.start();
+        var result = process.waitFor();
         if (result != 0) {
             var username = System.getProperty("user.name");
-            var errorOutput = new String(setupScript.getErrorStream().readAllBytes());
+            var errorOutput = new String(process.getErrorStream().readAllBytes());
             fail("Failed to cleanup SPI Test: \n" + errorOutput + "\n" +
                 "Probably you need to add the SPI bash script to sudoers file " +
-                "with visudo: '" + username + " ALL=(ALL) NOPASSWD: " + scriptPath.toFile().getParentFile().getAbsolutePath() + "/'");
+                "with visudo: '" + username + " ALL=(ALL) NOPASSWD: " + scriptPath.getParentFile().getAbsolutePath() + "/'");
         }
     }
 
