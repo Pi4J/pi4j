513e74ce9227be688921d865fb378215d192efe4
digitalsmile <digitalsmile.dig@gmail.com>
2026-06-11 14:26:42 +0300

    #643 test CI

--- plugins/pi4j-plugin-ffm/src/test/resources/pwm-setup.sh
+++ plugins/pi4j-plugin-ffm/src/test/resources/pwm-setup.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# Get the script to setup permissions
-bash <(curl -sSL https://raw.githubusercontent.com/Pi4J/pi4j-os/main/script/setup-permissions.sh) <<< y
-
 /bin/bash  ../native/pwm/build.sh
 insmod pwm-mock.ko
 
-# Sleep a second to let udev rules to be applied
-sleep 1
+udevadm trigger --verbose
 
-echo "123"
-ls -la /sys/class/pwm/pwmchip*
+# Sleep a second to let udev rules to be applied
+sleep 5
--- plugins/pi4j-plugin-ffm/src/test/resources/spi-setup.sh
+++ plugins/pi4j-plugin-ffm/src/test/resources/spi-setup.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# Get the script to setup permissions
-bash <(curl -sSL https://raw.githubusercontent.com/Pi4J/pi4j-os/main/script/setup-permissions.sh) <<< y
-
 /bin/bash  ../native/spi/build.sh
 modprobe spidev
 insmod spi-mock.ko
+
+udevadm trigger
 
 # Sleep a second to let udev rules to be applied
 sleep 1
